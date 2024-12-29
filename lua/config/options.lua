e007841d372059bb981f02e282c58f0ee1ed80f2
Carson <jackparadise520a@gmail.com>
2024-12-30 01:06:15 +0800

    預設啟動set list

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,6 +11,20 @@ function options.setup()
   vim.opt.wrap = false -- 禁止長行自動換行
 
   vim.g.editorconfig = false -- 預設是啟用的, 如果沒有禁用會得到: Error executing lua callback: root must be either "true" or "false"
+
+  -- set list
+  -- set nolist
+  vim.opt.list = true
+  vim.opt.listchars = {
+    -- tab = '🡢', -- 之後一定要再給一個空白，不然會錯
+    tab = '🡢 ', -- Tab 符號
+    -- space = '•',
+    trail = '·', -- 行尾多餘的空格
+    -- extends = '>', -- 行末的截斷符顯示為 >
+    -- precedes = '<', -- 行首的截斷符顯示為 <
+    -- eol = '⏎', -- 行結束位置
+    nbsp = ' ' -- U+00A0   non-breaking space
+  }
 end
 
 return options
