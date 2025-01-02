70e1d59092d7c13bc77bb25adccb8c6e46642958
Carson <jackparadise520a@gmail.com>
2025-01-02 18:08:46 +0800

    cursorcolumn=true, sursorColumn color

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,6 +10,28 @@ function options.setup()
 
   vim.opt.wrap = false -- 禁止長行自動換行
 
+  vim.wo.cursorcolumn = true -- 光標所在的整欄也會highlight
+
+  -- 檢查是否有支援真彩色
+  local supports_truecolor = vim.fn.getenv("COLORTERM") == "truecolor"
+  if supports_truecolor then
+    -- 真色彩
+    -- vim.cmd("highlight CursorColumn ctermbg=236 guibg=#3a3a3a") -- 光標所在欄顏色(預設是無)
+    -- 一次設定
+    vim.cmd([[
+        highlight CursorColumn ctermbg=236 guibg=#3a3a3a
+        highlight StatusLine guifg=#fefefe guibg=#282a36
+      ]])
+    --[[
+    highlight Visual guibg=#44475a
+    highlight Search guibg=#ffcc00 guifg=#000000
+    highlight DiagnosticError guifg=#ff5555
+    ]]--
+  else
+    -- 回退到256色
+    vim.cmd("highlight CursorColumn ctermbg=236")
+  end
+
   vim.g.editorconfig = false -- 預設是啟用的, 如果沒有禁用會得到: Error executing lua callback: root must be either "true" or "false"
 
   -- set list
