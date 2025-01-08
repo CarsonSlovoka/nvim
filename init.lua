c4eff81299e6bf52a8ed658500eed244903b1ed3
Carson <jackparadise520a@gmail.com>
2025-01-08 18:51:46 +0800

    ibl 採用更多顏色的配置

--- init.lua
+++ init.lua
@@ -704,10 +704,35 @@ vim.cmd('colorscheme github_dark_default')
 -- other
 
 -- other indent-blankline.nvim
-local plugin_indentBlankline
-status_ok, plugin_indentBlankline = pcall(require, "ibl") -- pack/other/start/indent-blankline.nvim/lua/ibl
+local plugin_ibl
+status_ok, plugin_ibl = pcall(require, "ibl") -- pack/other/start/indent-blankline.nvim/lua/ibl
 
 if status_ok then
-  plugin_indentBlankline.setup()
-end
+  -- plugin_ibl.setup() -- 如果想要最簡單的配置，這樣寫即可
 
+  local highlight = {
+    "RainbowRed",
+    "RainbowYellow",
+    "RainbowBlue",
+    "RainbowOrange",
+    "RainbowGreen",
+    "RainbowViolet",
+    "RainbowCyan",
+  }
+  local hooks = require "ibl.hooks"
+  hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+  end)
+
+  plugin_ibl.setup {
+    indent = {
+      highlight = highlight
+    }
+  }
+end
