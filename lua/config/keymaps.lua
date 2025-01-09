5335c4f6353bdd12739f1cd8b53375f51134ab7d
Carson <jackparadise520a@gmail.com>
2025-01-09 11:14:15 +0800

    keymap: <leader>fmt 格式化代碼 vim.lsp.buf.format 新增keymap.lua來簡寫設定

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,36 +1,48 @@
 local keymaps = {}
 
 local exec = require("utils.exec")
+local map = require("utils.keymap").keymap
+
 
 local function setup_normal()
-  -- 複製文件的絕對路徑
-  vim.keymap.set('n', -- normal mode
+  map('n',                       -- normal mode
     '<leader>cwd',
     ':let @+=expand("%:p")<CR>', -- % 表示當前的文件名, :p (轉成絕對路徑)
-    { noremap = true, silent = true }
+    { desc = "複製文件的絕對路徑" }
+  )
+
+  map('n', '<leader>fmt',
+    function()
+      vim.lsp.buf.format({
+        -- async = false, -- 可以用異步，這樣還可以去處理別的事，但是所想要明確的等待完成
+        timeout_ms = 3000,
+        -- range 有一個range的選項，可以只處理visual的那一部份，預設是對整個buffer都動作，檔案很大的時候可以考慮range
+      })
+      vim.notify("格式化結束", vim.log.levels.INFO)
+    end,
+    { desc = "格式化代碼" }
   )
 end
 
 local function setup_visual()
-  -- 將所有內容複製到剪貼簿
-  vim.keymap.set('v', -- Visual 模式
+  map('v',       -- Visual 模式
     '<leader>c', -- 快捷鍵為 <leader>c
-    '"+y', -- 將選中的內容複製到系統剪貼板
-    { noremap = true, silent = true }
+    '"+y',       --
+    { desc = "將選中的內容複製到系統剪貼板" }
   )
 
-  vim.keymap.set('x',
+  map('x',
     '<leader><F5>',
-  -- [[:lua ExecuteSelection()<CR>]],
+    -- [[:lua ExecuteSelection()<CR>]],
     function()
       vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true) -- 先離開visual模式，否則它會認為是在visual中運作，這會等到esc之後才會動作，導致你可能認為要按第二次才會觸發
-      vim.schedule(exec.ExecuteSelection) -- 並且使用 schedule 確保在模式更新後執行
+      vim.schedule(exec.ExecuteSelection)                                                          -- 並且使用 schedule 確保在模式更新後執行
     end,
-    { noremap = true, silent = true, desc="執行選中項目"}
+    { desc = "執行選中項目" }
   )
 
   -- 將工作目錄更改為當前檔案的目錄
-  vim.keymap.set("n", "<leader>cd", function()
+  map("n", "<leader>cd", function()
     local current_file = vim.fn.expand("%:p:h") -- 獲取當前檔案的目錄
     if current_file == "" then
       print("未打開任何檔案")
---
+++ lua/utils/keymap.lua
@@ -0,0 +1,11 @@
+local M = {}
+
+--- @param opts {noremap: boolean, silent: boolean, desc: string} desc如果沒有給，呈現的內容會放入cmd的指令
+function M.keymap(mode, key, cmd, opts)
+  opts = opts or {}
+  opts.noremap = opts.noremap ~= nil and opts.noremap or true
+  opts.silent = opts.silent ~= nil and opts.silent or true
+  vim.keymap.set(mode, key, cmd, opts)
+end
+
+return M
