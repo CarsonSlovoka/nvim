9ae053ec45a74597f36e2cf0a34679a9cb758bc6
Carson <jackparadise520a@gmail.com>
2025-01-30 00:13:51 +0800

    讓insert下一開始的輸入法是英文, 之後可再透過shift+ctrl去切換

--- lua/config/input.lua
+++ lua/config/input.lua
@@ -5,7 +5,12 @@ fcitx5-remote [OPTION]
   -c inactivate input method
   -o activate input method
   [no option] display fcitx state, 0 for close, 1 for inactive, 2 for active
---]]--
+
+顯示出當前輸入法的名稱，也就是取得<imname>
+fcitx5-remote -n
+ keyboard-us
+ boshiamy
+--]] --
 
 local Fcitx = {
   cmd = 'fcitx5-remote' -- 比較舊版的可能是fcitx-remote
@@ -14,7 +19,14 @@ local Fcitx = {
 function Fcitx.ActiveFcitx()
   local state = tonumber(vim.fn.system(Fcitx.cmd))
   if state ~= 2 then
-    vim.fn.system(Fcitx.cmd .. ' -o')
+    -- 採取的策略是統一換回英文，如果有需要在自己使用shift+ctrl去切換
+    -- 有寫與沒寫的差異是至少這樣可以確定一開始是英文, shift+ctrl可再切換成其它語言
+    -- 如果沒有寫，一開始無法確定語言，如果當時是boshiamy，那麼進入後還是boshimay
+
+    -- vim.fn.system(Fcitx.cmd .. ' -o') -- 可行，但是我們希望一開始進入的時候是使用英文，之後可以按下shift切換成自己要的輸入法
+    -- vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<S-Space><BS>", true, false, true), "n") 沒用，要單獨按下shift才會有用
+    -- vim.fn.system(Fcitx.cmd .. ' -s keyboard-us -o') -- 不需要這樣
+    vim.fn.system(Fcitx.cmd .. ' -s keyboard-us') -- -s 如果是指定非keyboard-us，例如boshiamy，即便目前是inActive的也會自動激活
     -- print("fcitx acitve")
   end
 end
