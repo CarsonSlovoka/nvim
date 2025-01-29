faa02678a3ad79d91127bc01d3ca4b543f49a973
Carson <jackparadise520a@gmail.com>
2025-01-29 17:34:58 +0800

    lualine path=4, 顯示目錄+檔名

--- init.lua
+++ init.lua
@@ -815,7 +815,32 @@ end
 local plugin_lualine
 status_ok, plugin_lualine = pcall(require, "lualine")
 if status_ok then
-  plugin_lualine.setup {}
+  plugin_lualine.setup {
+    sections = {
+      lualine_c = {
+        {
+          'filename',
+          -- 以下都是預設，其實可以直將path改成4即可
+          file_status = true,     -- Displays file status (readonly status, modified status)
+          newfile_status = false, -- Display new file status (new file means no write after created)
+          path = 4,               -- 0: Just the filename
+          -- 1: Relative path
+          -- 2: Absolute path
+          -- 3: Absolute path, with tilde as the home directory
+          -- 4: Filename and parent dir, with tilde as the home directory
+
+          shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+          -- for other components. (terrible name, any suggestions?)
+          symbols = {
+            modified = '[+]',      -- Text to show when the file is modified.
+            readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+            unnamed = '[No Name]', -- Text to show for unnamed buffers.
+            newfile = '[New]',     -- Text to show for newly created file before first write
+          }
+        }
+      },
+    }
+  }
 end
 
 
