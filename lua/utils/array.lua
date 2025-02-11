b87dafa32647a17547a7483df4c7933082f9d279
Carson <jackparadise520a@gmail.com>
2025-02-11 12:14:58 +0800

    ( update )

--- lua/utils/array.lua
+++ lua/utils/array.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 --- 將多個array整合成一個新的array
---- @param ... table[] 待合併的陣列集合
+--- @param ... table[]|number[] 待合併的陣列集合
 --- @return table 合併後的新陣列
 function M.Merge(...)
   local result = {}
