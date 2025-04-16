e835f3604d24c0c5b833ca14c2e49d635cc4fb2b
Carson <jackparadise520a@gmail.com>
2025-04-16 15:57:59 +0800

    utils.complete.get_file_only 僅獲取文件(不含目錄)

--- lua/utils/complete.lua
+++ lua/utils/complete.lua
@@ -11,4 +11,16 @@ function M.getDirOnly(argLead)
   return directories
 end
 
+---@param arg_lead string
+function M.get_file_only(arg_lead)
+  local all_file = vim.fn.getcompletion(arg_lead, "file") -- 包含檔案和目錄
+  local files = {}
+  for _, result in ipairs(all_file) do
+    if vim.loop.fs_stat(result) and vim.loop.fs_stat(result).type == "file" then
+      table.insert(files, result)
+    end
+  end
+  return files
+end
+
 return M
