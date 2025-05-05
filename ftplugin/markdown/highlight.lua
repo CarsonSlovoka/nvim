33b28823599e3113d10162bcb3656a629e3902f2
Carson <jackparadise520a@gmail.com>
2025-05-05 17:36:21 +0800

    @markup.{italic,strong,raw}.markdown_inline等顏色配置修改 vim.api.nvim_set_hl :Inspect

---
+++ ftplugin/markdown/highlight.lua
@@ -0,0 +1,26 @@
+-- 以下兩個沒用
+-- :echo synIDattr(synID(line("."), col("."), 1), "name")
+-- :lua print(vim.fn.synID(vim.fn.line("."), vim.fn.col("."), 1))
+
+-- :Inspect 可以查看出 vim.api.nvim_buf_add_highlight 所使用的顏色
+-- :Inspect
+-- vim.cmd("match YellowBold /\\*[^\\*]*\\*/") -- 用match的項目，無法被:Inspect查出來
+
+-- bg: #1e2727, #2b384c
+vim.api.nvim_set_hl(0, '@markup.italic.markdown_inline', { fg = '#ffffff', bg = "#1c2532", italic = true })
+vim.api.nvim_set_hl(0, '@markup.strong.markdown_inline', { fg = '#ffffff', bg = "#1c2532", bold = true })
+
+
+for _, hl in ipairs {
+  -- { fg = '#00e7e7', bg = '#1e2727', bold = true },
+  { fg = '#ffcb6b', bg = '#0d253f', bold = true },
+  -- { fg = '#c792ea', bold = true },
+  -- { fg = '#69ff94', bg = '#2e3440', bold = true },
+  -- { fg = '#ff5555', bold = true },
+  -- { fg = '#82aaff', bold = true },
+  -- { fg = '#f8f8f2', bg = '#2d1b4e', bold = true },
+  -- { fg = '#ff9f43', bg = '#252535', bold = true },
+} do
+  vim.api.nvim_set_hl(0, '@markup.raw.markdown_inline', { fg = hl.fg, bg = hl.bg, bold = hl.bold })
+  vim.api.nvim_set_hl(0, 'RenderMarkdownCodeInline', { fg = hl.fg, bg = hl.bg, bold = hl.bold })
+end
