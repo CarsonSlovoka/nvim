bc3b1129758c627c9ab242b6c1cc596c6bcccbcc
Carson <jackparadise520a@gmail.com>
2025-05-09 13:38:25 +0800

    🐛 修正有v使用visual_block接I或使用v接R時會多出m>的問題

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -103,7 +103,10 @@ function M.setup(opts)
 
   -- vim.keymap.set({ "v", "x" } -- x包含v, V. 但沒有Ctrl-V 而v會包含，並且包含所有x涵蓋的項目
   local enable_mark_range = true
-  for _, key in ipairs({ "c", ":" }) do
+  for _, key in ipairs({ "c", ":",
+    "I", -- 區塊選取時會用到
+    "R"  -- 取代時會用到，例如: 3Rf0 https://vi.stackexchange.com/a/25129/31859
+  }) do
     vim.keymap.set("v", key, function()
         enable_mark_range = false
         vim.defer_fn(function()
