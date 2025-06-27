30e131baadd83ed004498b39c8b6e3fde0210df2
Carson <jackparadise520a@gmail.com>
2025-06-27 13:25:05 +0800

    💪 對 filetype=opentype 新增ots相關工具 - ots-sanitize - ots-idempotent - ots-validator-checker - ots-side-by-side - ots-perf

--- lua/external/dap/opentype.lua
+++ lua/external/dap/opentype.lua
@@ -3,6 +3,60 @@
 --   command = "echo", -- 找一個不重要的指令, 為了通過require("dap")而已 -- 這個工具在 Linux / macOS / Windows shell 都有
 -- }
 
+-- -@type vim.SystemCompleted r
+
+---@param exe_name string
+---@param param table
+---@param ok_lines table 成功後的前導輸出文字
+---@param opts table
+local function show_run_result(exe_name, param, ok_lines, opts)
+  if vim.fn.executable(exe_name) == 0 then
+    return
+  end
+  local r = vim.system({ exe_name, unpack(param) }):wait()
+  if r.code ~= 0 then
+    vim.notify(string.format("❌ [%s] err code: %d err msg: %s", exe_name, r.code, r.stderr),
+      vim.log.levels.WARN)
+    return
+  end
+
+  vim.cmd("tabnew")
+  local buf = vim.api.nvim_get_current_buf()
+  vim.api.nvim_set_option_value("buftype", "nofile", { buf = buf })
+  -- local text = {
+  --   "file: " .. fontpath,
+  --   "result: ",
+  --   " ",
+  --   unpack(vim.split(r.stdout, "\n"))
+  -- }
+  local lines = ok_lines
+  for _, line in ipairs({
+    '',
+    string.format("cmd: %s %s", exe_name, table.concat(param, " ")),
+    '',
+  }) do
+    table.insert(lines, line)
+  end
+
+  table.insert(lines, "stdout:")
+  for _, line in ipairs(vim.split(r.stdout, "\n")) do
+    table.insert(lines, line)
+  end
+
+  table.insert(lines, "stderr:") -- 有些訊息也會寫在這邊，所以也有納入
+  for _, line in ipairs(vim.split(r.stderr, "\n")) do
+    table.insert(lines, line)
+  end
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+
+  for _, cmd in ipairs(opts.cmds) do
+    -- vim.cmd([[call matchadd('@label', '\v^\w*:')]]) -- 這樣的壞處是它是全域都有效的，不能指定buffer, 不過可以透過 :call clearmatches() 來全部清除
+    vim.cmd(cmd)
+  end
+  vim.cmd(string.format([[call matchadd('MiniIconsOrange', '%s')]], exe_name))
+end
+
+
 require("dap").configurations.opentype = {
   {
     name = "convert to fontTools:ttx format",
@@ -48,5 +102,101 @@ font._saveXML(writer)
       vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(r.stdout, "\n"))
       return ""
     end
-  }
+  },
+  {
+    name = "ots-sanitize 字型驗證器",
+    type = "custom",
+    request = "launch",
+    program = function()
+      local fontpath = vim.fn.expand("%:p")
+      local ok_lines = {
+        "file: " .. fontpath,
+      }
+      show_run_result("ots-sanitize", { fontpath },
+        ok_lines,
+        {
+          cmds = {
+            [[call matchadd('@label', '\v^\w*:')]]
+          }
+        }
+      )
+      return ""
+    end
+  },
+  {
+    name = "ots-idempotent 字型轉碼穩定性檢查器",
+    type = "custom",
+    request = "launch",
+    program = function()
+      local fontpath = vim.fn.expand("%:p")
+      local ok_lines = {
+        "file: " .. fontpath,
+      }
+      show_run_result("ots-idempotent", { fontpath },
+        ok_lines,
+        {
+          cmds = {
+            [[call matchadd('@label', '\v^\w*:')]]
+          }
+        }
+      )
+    end
+  },
+  {
+    name = "ots-validator-checker 惡意字型驗證測試工具",
+    type = "custom",
+    request = "launch",
+    program = function()
+      local fontpath = vim.fn.expand("%:p")
+      local ok_lines = {
+        "file: " .. fontpath,
+      }
+      show_run_result("ots-validator-checker", { fontpath },
+        ok_lines,
+        {
+          cmds = {
+            [[call matchadd('@label', '\v^\w*:')]]
+          }
+        }
+      )
+    end
+  },
+  {
+    name = "ots-side-by-side 渲染比對工具",
+    type = "custom",
+    request = "launch",
+    program = function()
+      local fontpath = vim.fn.expand("%:p")
+      local ok_lines = {
+        "file: " .. fontpath,
+      }
+      show_run_result("ots-side-by-side", { fontpath },
+        ok_lines,
+        {
+          cmds = {
+            [[call matchadd('@label', '\v^\w*:')]]
+          }
+        }
+      )
+    end
+  },
+  {
+    name = "ots-perf 轉碼效能測試工具",
+    type = "custom",
+    request = "launch",
+    program = function()
+      local fontpath = vim.fn.expand("%:p")
+      local ok_lines = {
+        "file: " .. fontpath,
+      }
+      show_run_result("ots-perf", { fontpath },
+        ok_lines,
+        {
+          cmds = {
+            [[call matchadd('@label', '\v^\w*:')]]
+          }
+        }
+      )
+    end
+  },
 }
