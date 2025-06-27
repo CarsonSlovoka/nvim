646f64ffaa74433ff9b45057ee4a5e18a68c0785
Carson <jackparadise520a@gmail.com>
2025-06-27 13:50:57 +0800

    不要直接對ttf, otf就進行otparser的動作, 加入到dap的選單讓使用者自行決定 將之前的實作搬移至 external/dap/opentype.lua 之中

--- lua/external/dap/opentype.lua
+++ lua/external/dap/opentype.lua
@@ -1,3 +1,5 @@
+local utils = require("utils.utils")
+
 -- require("dap").adapters.custome = {
 --   type = 'executable',
 --   command = "echo", -- 找一個不重要的指令, 為了通過require("dap")而已 -- 這個工具在 Linux / macOS / Windows shell 都有
@@ -56,8 +58,128 @@ local function show_run_result(exe_name, param, ok_lines, opts)
   vim.cmd(string.format([[call matchadd('MiniIconsOrange', '%s')]], exe_name))
 end
 
+local function program_otparser()
+  -- 確保執行檔存在
+  -- otparser.exe: https://github.com/CarsonSlovoka/otparser.nvim/blob/28c84b9320725582290a56d7c4af06c998d5495a/main.go#L59-L79
+  if vim.fn.executable("otparser") == 0 then
+    return
+  end
+
+  local fontPath = vim.fn.expand("%:p")
+  local fontname = "♻️" .. vim.fn.expand("%:t") -- 為了盡量避免與當前的buf同名，前面加上♻️ (如果要完全避免誤判，要額外記錄buffer id)
+  -- :echo expand("%:t") -- xxx.lua
+  -- :echo expand("%:e") -- lua
+
+  local exists, buf = utils.api.get_buf(vim.fn.getcwd() .. "/" .. fontname)
+  if not exists then
+    -- vim.api.nvim_command("vsplit enew")
+    vim.api.nvim_command("enew")
+    buf = vim.api.nvim_get_current_buf()
+    vim.api.nvim_set_option_value("buftype", "nofile", { buf = buf }) -- 設定為nofile就已經是不能編輯，但這只是代表可以編輯但是無法保存當前的檔案，但是可以用:w ~/other.txt 的方式來另儲
+    -- vim.api.nvim_set_option_value("bufhidden", "wipe", { buf = buf }) -- 不在buffer中記錄
+
+    -- vim.api.nvim_buf_set_name(buf, bufName) -- 注意！要給檔名就好
+    vim.api.nvim_buf_set_name(buf, fontname) -- 如果name是No Name時，使用vimgrep會遇到錯誤: E499: Empty file name for '%' or '#', only works with ":p:h" 因此為了能使vimgrep還是能有一個檔案的參照，需要設定其名稱
+    -- note: 使用nofile時再使用nvim_buf_set_name仍然有效，它會限制此檔案不能被保存
+    -- note: nvim_buf_set_name 的文件名稱，是在當前的工作目錄下建立此名稱
+    -- note: 如果buffer已經存在，會得到錯誤: Vim:E95: Buffer with this name already exists
+
+    vim.bo.filetype = "opentype"
+  elseif buf then
+    vim.api.nvim_set_current_buf(buf)
+  end
+
+  -- local output = vim.fn.system("otparser " .. vim.fn.shellescape(curFile)) -- 也行，但是建議用vim.system更明確
+  --- @type table
+  local r = vim.system({ "otparser", fontPath }):wait()
+  if r.code ~= 0 then -- 用回傳的code來當是否有錯的基準
+    vim.notify(string.format("❌ otparser error. err code: %d %s", r.code, r.stderr), vim.log.levels.WARN)
+    return
+  end
+
+  if buf then
+    -- -- vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(r.stdout, "\n")) -- 是可以直接寫在原本的地方，但是如果對原始的二進位有興趣，直接取代就不太好，所以另外開一個buffer寫
+    -- -- vim.api.nvim_buf_set_lines(0, 0, -1, false, { "hello", "world" })
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(r.stdout, "\n"))
+
+    -- vim.api.nvim_set_option_value("modifiable", false, { buf = buf }) -- readonly, 會直接連Insert都無法使用. 記得要放在nvim_buf_set_lines之後
+  end
+
+  if vim.fn.executable("xxd") == 0 then
+    return
+  end
+
+  -- 再建立一個新的buf來放xxd的結果
+  -- vim.cmd("vnew")
+  vim.cmd("vnew ++bin") -- 要補上++bin才可以讓%!xxd -r時得到原始的內容
+  vim.cmd("wincmd L")   -- 放到最右邊
+
+  -- { text = "head | 436 | 54" },
+  -- lua print(string.format("%x", 436)) -- 起始從00開始
+  -- lua print(string.format("%x", 436+54-1)) -- 不包含最後一個
+  buf = vim.api.nvim_get_current_buf()
+  local helps = {
+    'Tag  | Offset | Length',
+    'head | 436    | 54',
+    '436+',
+    'V53+',
+    '',
+    -- 可行，但是start, end輸入會被影響
+    -- 'let start = printf("%08x", 436)',
+    -- 'let end = printf("%08x", 436+53)',
+    -- "let cmd = printf('/%s/,/%s/yank', start, end)",
+    'let start = 436',
+    'let end = 436+53',
+    "let cmd = printf('/%s/,/%s/yank', printf('%08x', start), printf('%08x', end))",
+    ':execute cmd',
+    ':tabnew ++bin',
+    ':pu=@"',
+    '',
+    '',
+    ':lua print(string.format("%x", 436))',
+    ':lua print(string.format("%x", 436+53))',
+    ':/1b4/,/1e9/yank',
+    '',
+    '',
+    [[:call matchadd("Yellow", '\v^\d+.*')]], -- 需要\v才可以用^ -- ⚠️ 在vim上用單引號，雙引號會不行
+    [[:let m = matchadd("Blue", '\v^\d+')]],
+    ':call matchdelete(m)',
+    '',
+    '',
+    "'<,'>Highlight YellowBold *",
+    ":copen 5",
+    "%!xxd -r",
+    "54GoSelect 437",
+    ' ', -- 這個用來放xxd的內容
+  }
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, helps)
+  local ns_id = vim.api.nvim_create_namespace("hightlight_comment")
+  vim.hl.range(buf, ns_id, "Comment", { 0, 0 }, { #helps, -1 }) -- ns_id不可以用0，一定要建立
+  vim.cmd("normal! G")
+
+  -- vim.cmd("r !xxd -c 1 " .. fontPath) -- 這個會有:mes的訊息，如果不想要，可以用vim.fn.systemlist來代替
+  local output = vim.fn.systemlist("xxd -c 1 " .. fontPath)
+  local row = vim.api.nvim_win_get_cursor(0)[1]
+  vim.api.nvim_buf_set_lines(0, row, row, false, output)
+
+  -- vim.fn.setloclist(0, {
+  vim.fn.setqflist({ -- 這有可能在其它的buffer也需要參考，所以用qflist會比較好點
+    { text = "vnew ++bin" },
+    { text = "r !xxd -c 1 " .. fontPath },
+    { text = "r !xxd -c 16 " .. fontPath },
+    { text = "%!xxd -r" },
+  }, 'a')
+
+  vim.api.nvim_set_option_value("buftype", "nofile", { buf = buf })
+end
 
 require("dap").configurations.opentype = {
+  {
+    name = "otparser",
+    type = "custom",
+    request = "launch",
+    program = program_otparser,
+  },
   {
     name = "convert to fontTools:ttx format",
     type = "custom",
