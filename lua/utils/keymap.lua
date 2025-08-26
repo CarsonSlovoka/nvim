abc9f8922c223a73942b80a48832f9ee7dd0cfad
Carson <jackparadise520a@gmail.com>
2025-08-26 11:14:04 +0800

    ( 預設用remap: false )

--- lua/utils/keymap.lua
+++ lua/utils/keymap.lua
@@ -1,6 +1,6 @@
 local M = {}
 
---- @param opts {noremap: boolean, silent: boolean, desc: string} desc如果沒有給，呈現的內容會放入cmd的指令
+--- @param opts {remap: boolean, silent: boolean, desc: string} desc如果沒有給，呈現的內容會放入cmd的指令
 function M.keymap(mode, key, cmd, opts)
   opts = opts or {}
   -- opts.noremap = opts.noremap ~= nil and opts.noremap or true -- true不會再往下傳遞
