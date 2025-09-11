e9a1f0be5e1d4c2573454786cc5c740b0728cdc6
Carson <jackparadise520a@gmail.com>
2025-09-11 17:41:50 +0800

    將 image.nvim 的設定改為: at_cursor = true, cursor_mode = inline

--- init.lua
+++ init.lua
@@ -2074,10 +2074,10 @@ local function install_image()
         enabled = true,
         clear_in_insert_mode = false,
         download_remote_images = true,
-        only_render_image_at_cursor = false,
-        only_render_image_at_cursor_mode = "popup",
-        floating_windows = false,              -- if true, images will be rendered in floating markdown windows
-        filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+        only_render_image_at_cursor = true,
+        only_render_image_at_cursor_mode = "inline", -- popup, inline
+        floating_windows = false,                    -- if true, images will be rendered in floating markdown windows
+        filetypes = { "markdown", "vimwiki" },       -- markdown extensions (ie. quarto) can go here
       },
       neorg = {
         enabled = true,
