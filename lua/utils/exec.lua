e6266ec3e754576818ee6274cc1c489a5416896a
Carson <jackparadise520a@gmail.com>
2025-10-02 14:53:41 +0800

    可以支持直接給予要被sh -c所執行的內容

--- lua/utils/exec.lua
+++ lua/utils/exec.lua
@@ -1,29 +1,33 @@
 local exec = {}
 
-function exec.ExecuteSelection()
-  -- 獲取選取範圍的起始和結束位置
-  local start_pos = vim.fn.getpos("'<")
-  local end_pos = vim.fn.getpos("'>")
+---@param cmd string? The command to be executed in sh -c
+function exec.ExecuteSelection(cmd)
+  local command = cmd
+  if command == nil then
+    -- 獲取選取範圍的起始和結束位置
+    local start_pos = vim.fn.getpos("'<")
+    local end_pos = vim.fn.getpos("'>")
 
-  local start_row, start_col = start_pos[2], start_pos[3]
-  local end_row, end_col = end_pos[2], end_pos[3]
+    local start_row, start_col = start_pos[2], start_pos[3]
+    local end_row, end_col = end_pos[2], end_pos[3]
 
-  -- 獲取選中的行內容
-  local lines = vim.fn.getline(start_row, end_row)
-  if not lines or #lines == 0 then
-    return -- 如果選取內容為空，直接結束
+    -- 獲取選中的行內容
+    local lines = vim.fn.getline(start_row, end_row)
+    if not lines or #lines == 0 then
+      return -- 如果選取內容為空，直接結束
+    end
+
+    -- 修正第一行和最後一行的選取範圍
+    if #lines == 1 then
+      lines[1] = string.sub(lines[1], start_col, end_col)
+    else
+      lines[1] = string.sub(lines[1], start_col)
+      lines[#lines] = string.sub(lines[#lines], 1, end_col)
+    end
+
+    -- 合併行內容為單一命令字串
+    command = table.concat(lines, "\n")
   end
-
-  -- 修正第一行和最後一行的選取範圍
-  if #lines == 1 then
-    lines[1] = string.sub(lines[1], start_col, end_col)
-  else
-    lines[1] = string.sub(lines[1], start_col)
-    lines[#lines] = string.sub(lines[#lines], 1, end_col)
-  end
-
-  -- 合併行內容為單一命令字串
-  local command = table.concat(lines, "\n")
   print(command)
 
   -- 非同步執行命令，無輸出到文本
