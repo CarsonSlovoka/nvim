1227a9f63403a4b535bd6207b57947abf26455ed
Carson <jackparadise520a@gmail.com>
2025-10-02 16:37:25 +0800

    🚀 可以顯示 stdout, stderr 的訊息; jobstart 參考
    vim.fn.jobstart

--- lua/utils/exec.lua
+++ lua/utils/exec.lua
@@ -28,20 +28,95 @@ function exec.ExecuteSelection(cmd)
     -- 合併行內容為單一命令字串
     command = table.concat(lines, "\n")
   end
-  print(command)
+  print(command) -- NOTE: print 如果太長的時候，就需要按下Enter才可以繼續
 
   -- 非同步執行命令，無輸出到文本
   local curDir = vim.fn.expand("%:p:h")
-  vim.uv.spawn("sh", {
-    args = { "-c", command },
-    cwd = curDir -- (optional) 設定工作目錄
-  }, function(code, signal)
-    if code ~= 0 then
-      vim.schedule(function()
-        print("執行失敗，錯誤代碼:", code, "信號:", signal)
-      end)
-    end
-  end)
+
+  -- 以下的作法沒辦法呈現stdout真實的內容
+  -- vim.uv.spawn("sh", {
+  --   args = { "-c", command },
+  --   cwd = curDir
+  -- }, function(code, signal)
+  --   if code ~= 0 then
+  --     vim.schedule(function()
+  --       print("執行失敗，錯誤代碼:", code, "信號:", signal)
+  --     end)
+  --   end
+  -- end)
+
+
+  -- 以下方法可行，但是用jobstart會比較簡潔
+  -- local stdout = vim.uv.new_pipe(false) -- 建立 stdout pipe
+  -- local function on_stdout(err, data)
+  --   if err then
+  --     vim.schedule(function()
+  --       print("stdout 讀取錯誤:", err)
+  --     end)
+  --     return
+  --   end
+  --   if data then
+  --     vim.schedule(function()
+  --       -- print("stdout 輸出:", vim.inspect(data)) -- 輸出 stdout 內容. 有個缺點，如果輸出有\n不會自動換行顯示，都會合在一列
+  --       vim.api.nvim_echo({ { data, "Normal" }, }, false, {}) -- TIP: 輸出有\n, 會換行
+  --     end)
+  --   end
+  -- end
+  --
+  -- vim.uv.spawn("sh", {
+  --   args = { "-c", command },
+  --   cwd = curDir,
+  --   stdio = { nil, stdout, nil } -- 設定 stdio：stdin, stdout, stderr
+  -- }, function(code, signal)
+  --   stdout:read_stop()
+  --   stdout:close()
+  --   if code ~= 0 then
+  --     vim.schedule(function()
+  --       print("執行失敗，錯誤代碼:", code, "信號:", signal)
+  --     end)
+  --   end
+  -- end)
+  -- stdout:read_start(on_stdout) -- 啟動讀取 stdout
+
+  -- vim.fn.jobstart("sh -c " .. command, -- WARN: command要用''包起來不然執行的指令會不完整！
+  -- vim.cmd("tabnew | setlocal buftype=nofile")
+  -- vim.fn.jobstart(string.format("sh -c %q", command), -- 這個多列的情況有的會有問題
+  vim.fn.jobstart(string.format("sh -c '\n%s\n'", command),
+    {
+      -- term = true, -- 如果用term，只要前面先用tabnew之後的stdout, stderr都可以直接在終端輸出，不需要再自己處理，只是最後會需要再按一次Enter來結束, 不過也是可以在on_exit時用nvim_buf_delete來刪除，但是輸出的訊息就會來不急看
+      cwd = curDir,           -- (optional) 設定工作目錄
+      stdout_buffered = true, -- 緩衝 stdout，一次性傳給回調
+      stderr_buffered = true,
+      on_stdout = function(_, data, _)
+        -- if #data == 0 then return end -- 這不準，有的是 { "" }
+        if #data == 0 or (#data == 1 and data[1] == "") then
+          return
+        end
+        vim.api.nvim_echo({
+          { "stdout\n",               "@label" },
+          { table.concat(data, "\n"), "Normal" }, -- TIP: 輸出有\n, 會換行
+        }, false, {})
+      end,
+      on_stderr = function(_, data, _)
+        if #data == 0 or (#data == 1 and data[1] == "") then
+          return
+        end
+        vim.api.nvim_echo({
+          { "stderr\n",               "@label" },
+          -- { "❌ " .. table.concat(data, "\n"), "Normal" }, -- 不見得stderr就是有問題，有的程式會有stdout, stderr都會輸出
+          { table.concat(data, "\n"), "Normal" },
+        }, false, {})
+      end,
+
+      -- -- 就算定義了on_stdout, on_stderr, 還是可以再處理on_exit, 但覺得不太需要
+      -- on_exit = function(id, code, signal)
+      --   if code ~= 0 then
+      --     print("執行失敗，錯誤代碼:", code, "信號:", signal, id)
+      --   else
+      --     print("執行成功！")
+      --   end
+      -- end,
+    })
 end
 
 return exec
