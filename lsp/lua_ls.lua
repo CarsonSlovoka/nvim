739be95b0df604252aa5dc42e9ce9404bb2eef2f
Carson <jackparadise520a@gmail.com>
2025-10-15 13:54:36 +0800

    ( update )

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -16,9 +16,8 @@ vim.lsp.config('lua_ls', {
   on_init = function(client)
     if client.workspace_folders then
       local path = client.workspace_folders[1].name
-      if
-        path ~= vim.fn.stdpath('config')
-        and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+      if path ~= vim.fn.stdpath('config')
+          and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
       then
         return
       end
