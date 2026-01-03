d58f18c5439ca47d5e55e95b2ff2fcc97076fb13
Carson <jackparadise520a@gmail.com>
2026-01-03 12:53:18 +0800

    嘗試配置bun的debug: 失敗

--- lua/external/dap/javascript.lua
+++ lua/external/dap/javascript.lua
@@ -25,12 +25,48 @@ require("dap").adapters["pwa-node"] = {
   }
 }
 
-require("dap").configurations.javascript = {
-  {
-    type = "pwa-node",
-    request = "launch",
-    name = "Launch file",
-    program = "${file}",
-    cwd = "${workspaceFolder}",
-  },
-}
+for _, filetype in ipairs({
+  "javascript",
+  "typescript",
+}) do
+  require("dap").configurations[filetype] = {
+    {
+      name = "Launch file",
+      type = "pwa-node",
+      request = "launch",
+      program = "${file}",
+      cwd = "${workspaceFolder}",
+    },
+    -- bun 都沒有試成功
+    -- {
+    --   name = "Launch Bun",
+    --   type = "pwa-node",
+    --   request = "launch",
+    --   runtimeExecutable = "bun",
+    --   runtimeArgs = { "run", "--inspect-brk", "${file}" },
+    --   port = function()
+    --     return vim.fn.input("port: ")
+    --   end,
+    --   rootPath = vim.fn.getcwd(),
+    --   cwd = vim.fn.getcwd(),
+    --   console = "integratedTerminal",
+    --   -- internalConsoleOptions = "neverOpen",
+    --   -- protocol = "ws",
+    -- },
+    -- {
+    --   name = "Attach to Bun",
+    --   type = "pwa-node",
+    --   request = "attach",
+    --   port = 6499, -- Bun 預設偵錯埠是 6499
+    --   -- port = function()
+    --   --   return vim.fn.input("port: ")
+    --   -- end,
+    --   rootPath = vim.fn.getcwd(),
+    --   cwd = vim.fn.getcwd(),
+    --   restart = true, -- 自動重新連接
+    --   -- protocol = "inspector",
+    --   protocol = "ws", -- 換這個也是失敗
+    --   sourceMaps = true,
+    -- },
+  }
+end
