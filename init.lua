367bde97d66fb0c920a8f4b3aa46578a2a011382
Carson <jackparadise520a@gmail.com>
2026-01-16 10:22:27 +0800

    nvim-treesitter-textobjects 新版配置

--- init.lua
+++ init.lua
@@ -226,6 +226,133 @@ local function install_nvimTreesitterContext()
   }
 end
 
+local function install_nvimTreesitterTextobjects()
+  if not pcall(require, "nvim-treesitter-textobjects") then
+    vim.notify("Failed to load nvim-treesitter-textobjects", vim.log.levels.WARN)
+    return
+  end
+
+  -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects/blob/baa6b4ec28c8be5e4a96f9b1b6ae9db85ec422f8/README.md?plain=1#L43-L163
+  require("nvim-treesitter-textobjects").setup {
+    select = {
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+      -- You can choose the select mode (default is charwise 'v')
+      --
+      -- Can also be a function which gets passed a table with the keys
+      -- * query_string: eg '@function.inner'
+      -- * method: eg 'v' or 'o'
+      -- and should return the mode ('v', 'V', or '<c-v>') or a table
+      -- mapping query_strings to modes.
+      selection_modes = {
+        ['@parameter.outer'] = 'v', -- charwise
+        ['@function.outer'] = 'V',  -- linewise
+        -- ['@class.outer'] = '<c-v>', -- blockwise
+      },
+      -- If you set this to `true` (default is `false`) then any textobject is
+      -- extended to include preceding or succeeding whitespace. Succeeding
+      -- whitespace has priority in order to act similarly to eg the built-in
+      -- `ap`.
+      --
+      -- Can also be a function which gets passed a table with the keys
+      -- * query_string: eg '@function.inner'
+      -- * selection_mode: eg 'v'
+      -- and should return true of false
+      include_surrounding_whitespace = false,
+    },
+    move = {
+      -- whether to set jumps in the jumplist
+      set_jumps = true
+    }
+  }
+
+  -- keymaps: select
+  -- You can use the capture groups defined in `textobjects.scm`
+  local select = require "nvim-treesitter-textobjects.select"
+  vim.keymap.set({ "x", "o" }, "am", function()
+    select.select_textobject("@function.outer", "textobjects")
+  end)
+  vim.keymap.set({ "x", "o" }, "im", function()
+    select.select_textobject("@function.inner", "textobjects")
+  end)
+  vim.keymap.set({ "x", "o" }, "ac", function()
+    select.select_textobject("@class.outer", "textobjects")
+  end)
+  vim.keymap.set({ "x", "o" }, "ic", function()
+    select.select_textobject("@class.inner", "textobjects")
+  end)
+  -- You can also use captures from other query groups like `locals.scm`
+  vim.keymap.set({ "x", "o" }, "as", function()
+    select.select_textobject("@local.scope", "locals")
+  end)
+
+  -- keymaps: swap
+  local swap = require("nvim-treesitter-textobjects.swap")
+  vim.keymap.set("n", "<leader>a", function()
+    swap.swap_next "@parameter.inner"
+  end)
+  vim.keymap.set("n", "<leader>A", function()
+    swap.swap_previous "@parameter.outer"
+  end)
+
+  -- keymaps: move
+  local move = require("nvim-treesitter-textobjects.move")
+  vim.keymap.set({ "n", "x", "o" }, "]m", function()
+    move.goto_next_start("@function.outer", "textobjects")
+  end)
+  vim.keymap.set({ "n", "x", "o" }, "]]", function()
+    move.goto_next_start("@class.outer", "textobjects")
+  end)
+  -- You can also pass a list to group multiple queries.
+  vim.keymap.set({ "n", "x", "o" }, "]o", function()
+    move.goto_next_start({ "@loop.inner", "@loop.outer" }, "textobjects")
+  end)
+  -- You can also use captures from other query groups like `locals.scm` or `folds.scm`
+  vim.keymap.set({ "n", "x", "o" }, "]s", function()
+    move.goto_next_start("@local.scope", "locals")
+  end)
+  vim.keymap.set({ "n", "x", "o" }, "]z", function()
+    move.goto_next_start("@fold", "folds")
+  end)
+
+  vim.keymap.set({ "n", "x", "o" }, "]M", function()
+    move.goto_next_end("@function.outer", "textobjects")
+  end)
+  vim.keymap.set({ "n", "x", "o" }, "][", function()
+    move.goto_next_end("@class.outer", "textobjects")
+  end)
+
+  vim.keymap.set({ "n", "x", "o" }, "[m", function()
+    move.goto_previous_start("@function.outer", "textobjects")
+  end)
+  vim.keymap.set({ "n", "x", "o" }, "[[", function()
+    move.goto_previous_start("@class.outer", "textobjects")
+  end)
+
+  vim.keymap.set({ "n", "x", "o" }, "[M", function()
+    move.goto_previous_end("@function.outer", "textobjects")
+  end)
+  vim.keymap.set({ "n", "x", "o" }, "[]", function()
+    move.goto_previous_end("@class.outer", "textobjects")
+  end)
+
+  -- Go to either the start or the end, whichever is closer.
+  -- Use if you want more granular movements
+  vim.keymap.set({ "n", "x", "o" }, "]d", function()
+    move.goto_next("@conditional.outer", "textobjects")
+  end)
+  vim.keymap.set({ "n", "x", "o" }, "[d", function()
+    move.goto_previous("@conditional.outer", "textobjects")
+  end)
+
+
+  local ts_repeat_move = require "nvim-treesitter-textobjects.repeatable_move"
+  -- Repeat movement with ; and ,
+  -- ensure ; goes forward and , goes backward regardless of the last direction
+  vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+  vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
+end
+
 local function install_lspconfig()
   -- ⭐ 如果你的neovim是透過source來生成，那麼所有內建的lua都會被放到 /usr/share/nvim/runtime/lua 目錄下，例如:
   --        ~/neovim/runtime/lua/vim/lsp.lua  # 假設你的neovim是clone到家目錄下，那麼此lsp.lua由source建立完成之後，就會被放到以下的目錄
@@ -2395,10 +2522,11 @@ local installs = {
     end,
     delay = 0
   },
-  { name = "nvimTreesitter",     fn = install_nvimTreesitter,        delay = 0 },
-  { name = "treesitter-context", fn = install_nvimTreesitterContext, delay = 0 },
+  { name = "nvimTreesitter",              fn = install_nvimTreesitter,            delay = 0 },
+  { name = "nvim-treesitter-textobjects", fn = install_nvimTreesitterTextobjects, delay = 0 },
+  { name = "treesitter-context",          fn = install_nvimTreesitterContext,     delay = 0 },
 
-  { name = "lspconfig",          fn = install_lspconfig,             delay = 0 },
+  { name = "lspconfig",                   fn = install_lspconfig,                 delay = 0 },
   {
     name = "lspconfig",
     fn = function()
