5e3e19ad44b4847872d23a53cc24483e9c3d9aa3
Carson <jackparadise520a@gmail.com>
2026-02-02 18:18:52 +0800

    ( 設定 vim.go.ignorecase = true 使得leap.nvim連第二按鍵也可以不區分大小寫 )

--- init.lua
+++ init.lua
@@ -569,8 +569,10 @@ local function install_leap()
     vim.notify("Failed to load leap", vim.log.levels.ERROR)
     return
   end
+  vim.go.ignorecase = true
   require('leap').setup({
-    case_sensitive = false, -- 第一鍵不區分大小寫, 第二個按鍵還是會分
+    -- cd pack/motion/start/leap.nvim && git show f19d4359:lua/leap/main.lua | bat -l lua -P -r 79:83
+    case_sensitive = false, -- 第一鍵不區分大小寫, 第二個按鍵還是會分, 如果要第二鍵不分要讓vim.go.ignorecase為true
   })
 
   vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
