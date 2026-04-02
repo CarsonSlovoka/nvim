4471a8a78b1ffbe8dc493c8f38801f3c0dc198d5
Carson <jackparadise520a@gmail.com>
2026-04-02 15:10:23 +0800

    🐛 解決 `:checkhealth nvim-treesitter` 中顯示的H一直無法出現的問題. (區塊代碼無法高亮)
    用 `:TSUninstall all` 之後重啟nvim讓其重裝即可
    如此markdown中的code-block的顏色就會顯示

--- plugin/nvim-treesitter.lua
+++ plugin/nvim-treesitter.lua
@@ -1,24 +1,29 @@
+-- Important: 如果發現: `:checkhealth nvim-treesitter` 的H一直沒有打勾，可以嘗試: `TSUninstall all`之後重新，再讓它重新安裝所有項目即可
+
 -- local function ts_disable(_, bufnr)
 --   return vim.api.nvim_buf_line_count(bufnr) > 10000
 -- end
 
+-- ~/.local/share/nvim/site/pack/core/opt/nvim-treesitter/
 require("nvim-treesitter").setup({
-  -- `:lua print(vim.fn.stdpath('data') .. '/site/pack/core/opt/')`
-  -- ~/.local/share/nvim/site/pack/core/opt/
+  -- `:lua print(vim.fn.stdpath('data') .. '/site')`
+  -- ~/.local/share/nvim/site
   install_dir = vim.fn.stdpath("data") .. "/site",
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-    -- disable = function(_, bufnr)
-    --   return ts_disable(_, bufnr)
-    -- end,
-  },
-  indent = { enable = true },
-  fold = {
-    -- vim.opt.foldmethod = "expr"
-    -- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-    enable = true,
-  }
+
+  -- 👇 以下配置從 Neovim 0.12 + nvim-treesitter main 開始不再有效
+  -- highlight = {
+  --   enable = true,
+  --   additional_vim_regex_highlighting = false,
+  --   -- disable = function(_, bufnr)
+  --   --   return ts_disable(_, bufnr)
+  --   -- end,
+  -- },
+  -- indent = { enable = true },
+  -- fold = {
+  --   -- vim.opt.foldmethod = "expr"
+  --   -- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+  --   enable = true,
+  -- }
 })
 
 local parsers = {
@@ -60,3 +65,9 @@ vim.api.nvim_create_autocmd("FileType", {
     end
   end
 })
+
+
+-- vim.api.nvim_create_autocmd('FileType', {
+--   pattern = { '<filetype>' },
+--   callback = function() vim.treesitter.start() end,
+-- })
