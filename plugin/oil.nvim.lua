bfabaffc3a074c921e43f41edf6fb54f13aa5324
Carson <jackparadise520a@gmail.com>
2026-05-02 19:43:30 +0800

    keymap: <leader>e  :Oil

--- plugin/oil.nvim.lua
+++ plugin/oil.nvim.lua
@@ -2,3 +2,7 @@ vim.pack.add({ "https://github.com/stevearc/oil.nvim" })
 require("oil").setup()
 
 -- Tip: `:verbose command Oil` 可以直接曉得這個指令實作在什麼位置
+
+-- vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }) -- 只用 - 會和往上移動N行衝突
+vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+-- vim.keymap.set("n", "<leader>e", "<CMD>Oil --float<CR>", { desc = "Oil Float" })
