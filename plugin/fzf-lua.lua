d8338083de3f7bc2df7312cb6dd9373f8df3a6fe
Carson <jackparadise520a@gmail.com>
2026-05-07 09:47:47 +0800

    ( reformat )

--- plugin/fzf-lua.lua
+++ plugin/fzf-lua.lua
@@ -5,7 +5,7 @@ require("fzf-lua").setup({
     row = 10,
     col = 0,
     preview = {
-      hidden = false,   -- 啟動時顯示預覽
+      hidden = false, -- 啟動時顯示預覽
     },
     fullscreen = true,
   },
@@ -17,13 +17,13 @@ require("fzf-lua").setup({
       -- ['<A-l>'] = 'preview-page-right',
       ['<A-p>'] = 'preview-page-up',
       ['<A-n>'] = 'preview-page-down',
-      ['<C-t>'] = 'toggle-preview',   -- 用 Ctrl+T 來 toggle 預覽視窗（隱藏/顯示）
+      ['<C-t>'] = 'toggle-preview', -- 用 Ctrl+T 來 toggle 預覽視窗（隱藏/顯示）
     },
   },
   buffers = {
     actions = {
       -- ["alt-d"] = require("fzf-lua.actions").buf_del, -- 刪除 buffer, 但之後離開視窗了
-      ["alt-d"] = function(selected, opts)   -- 使其可以像require("telescope.builtin").buffers那樣也可以用alt-d來刪除
+      ["alt-d"] = function(selected, opts) -- 使其可以像require("telescope.builtin").buffers那樣也可以用alt-d來刪除
         require("fzf-lua.actions").buf_del(selected, opts)
 
         -- 再重新載入 buffer 清單，保持 fzf 視窗不關閉
@@ -32,9 +32,9 @@ require("fzf-lua").setup({
     },
     winopts = {
       preview = {
-        vertical = "down:50%",   -- preview 顯示在下方，高度 50%（可調整）
+        vertical = "down:50%", -- preview 顯示在下方，高度 50%（可調整）
         -- border = "rounded",    -- 邊框樣式（可選）
-        layout = "vertical",     -- 確保使用垂直佈局 👈 這個才是將preview, 放在下方的關鍵
+        layout = "vertical",   -- 確保使用垂直佈局 👈 這個才是將preview, 放在下方的關鍵
       },
     },
   }
