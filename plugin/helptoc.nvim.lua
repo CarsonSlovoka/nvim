09e2213b776c117eb843888bcce698b831084657
Carson <jackparadise520a@gmail.com>
2026-06-30 10:29:26 +0800

    upgrade: helptoc
    - 只要有 lsp 的項目都可以使用
    - 新增 kind_icons
    - kind_icons 能有顏色突顯

--- plugin/helptoc.nvim.lua
+++ plugin/helptoc.nvim.lua
@@ -1,26 +1,31 @@
 vim.defer_fn(function()
   vim.pack.add({ "https://github.com/CarsonSlovoka/helptoc.nvim" })
   -- vim.cmd.packadd("helptoc.nvim") -- 地端開發
+  local m = require("helptoc")
 
-  -- require("helptoc").setup()
-  require("helptoc").setup({
+  -- m.setup()
+  m.setup({
     -- indent_size = 'auto', -- tree, auto
     -- position = "right"
     highlight = {
       -- cursor_line = { link = "@Label" }
       cursor_line = { bg = vim.g.terminal_color_4 or "#00c6ff", fg = "#003b4f" }
-    }
+    },
+    -- enable = {
+    --   kind_icon = true,
+    -- symbol_highlight = true,
+    -- }
   })
 
-  local group = "HelpToc"
-  vim.api.nvim_create_augroup(group, {})
+  -- local group = "HelpToc"
+  -- vim.api.nvim_create_augroup(group, {})
   vim.api.nvim_create_autocmd("FileType", {
-    group = group,
-    pattern = {
-      "markdown",
-      "sh", "zsh",
-      "lua"
-    },
+    group = m.group,
+    -- pattern = {
+    --   "markdown",
+    --   "sh", "zsh",
+    --   "lua"
+    -- },
     callback = function()
       local buf = vim.api.nvim_get_current_buf()
       vim.keymap.set("n", "<leader>h", "<cmd>Helptoc<CR>", { noremap = true, silent = true, buf = buf })
