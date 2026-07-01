9d151558805ec12942e8043f8745d0d49618320b
Carson <jackparadise520a@gmail.com>
2026-07-01 15:23:55 +0800

    upgrade: helptoc
    - 支持所有 lsp_kinds: https://github.com/microsoft/language-server-protocol/blob/ad04bde24d0c3850dcb6ec08e802f7e69c2ee5dc/_specifications/specification-3-16.md?plain=1#L4754-L4780
    - lsp_kinds 篩選

--- plugin/helptoc.nvim.lua
+++ plugin/helptoc.nvim.lua
@@ -1,4 +1,7 @@
 vim.defer_fn(function()
+  -- Tip: pack.del會直接刪除 `:lua vim.fn.setreg('a', vim.fn.stdpath('data') .. '/site/pack/core/opt/')` 底下所對應的文件夾
+  -- vim.pack.del({ "helptoc.nvim" }) -- 可以更新 ../nvim-pack-lock.json 之後刪除重新下載. -- Note: 如果這個目錄沒有會直接報錯
+  -- Note: 如果要直接抓最新版本的，也可以直接將 ../nvim-pack-lock.json 所對應的項目刪除, 預設會直接抓最後一版本然後更新
   vim.pack.add({ "https://github.com/CarsonSlovoka/helptoc.nvim" })
   -- vim.cmd.packadd("helptoc.nvim") -- 地端開發
   local m = require("helptoc")
