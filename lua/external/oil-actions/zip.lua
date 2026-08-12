fee6578b9f715d26e42f35a719072e0733be8ecb
Carson <jackparadise520a@gmail.com>
2026-08-12 11:23:07 +0800

    print出使用的指令

--- lua/external/oil-actions/zip.lua
+++ lua/external/oil-actions/zip.lua
@@ -3,13 +3,14 @@ return {
     label = "Unzip here",
     callback = function(path)
       local dir = vim.fn.fnamemodify(path, ":h")
+      local cmd = { "unzip", "-o", path, "-d", dir }
       vim.system(
-        { "unzip", "-o", path, "-d", dir },
+        cmd,
         { text = true, cwd = dir },
         function(obj)
           vim.schedule(function()
             if obj.code == 0 then
-              vim.notify(path .. " : Unzipped successfully", vim.log.levels.INFO)
+              vim.notify(table.concat(cmd, " ") .. "\n ✅ Unzipped successfully", vim.log.levels.INFO)
               -- 重新整理 oil 視窗
               require("oil.actions").refresh.callback()
             else
